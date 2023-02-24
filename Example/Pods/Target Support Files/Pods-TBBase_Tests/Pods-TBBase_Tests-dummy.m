ac0a7217d46a7cc8d855e69f731de7dd25902bd5
taobo <691075311@qq.com>
2023-02-24 13:56:17 +0800

    dele

--- Example/Pods/Target Support Files/Pods-TBBase_Tests/Pods-TBBase_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TBBase_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TBBase_Tests
-@end
--- Example/Pods/Target Support Files/TBBase/TBBase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TBBase : NSObject
-@end
-@implementation PodsDummy_TBBase
-@end
